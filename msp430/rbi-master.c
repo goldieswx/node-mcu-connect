0bf6806cf737caf79f454825ac0926e3dcf6e7b3
David Jakubowski <goldieswx@github.com>
2014-06-21 13:39:21 +0200

    sncc procedure done, fixed compiling errors, ready for testing

--- msp430/rbi-master.c
+++ msp430/rbi-master.c
@@ -1,19 +1,19 @@
 /*
-    node-mcu-connect . node.js UDP Interface for embedded devices.
-    Copyright (C) 2013-4 David Jakubowski
+	node-mcu-connect . node.js UDP Interface for embedded devices.
+	Copyright (C) 2013-4 David Jakubowski
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>. 
 */
 
 #include <bcm2835.h>
@@ -47,7 +47,9 @@
 #define PORT 9930
 
 #define MI_STATUS_QUEUED 0x03
-#define MI_STATUS_TRANSFERRED 0X04
+#define MI_STATUS_TRANSFERRED 0x04
+#define SNCC_SIGNAL_RECEIVED  0x05
+#define SNCC_PREAMBLE_RECEIVED 0x06
 
 #define MCOM_DATA_LEN 20
 #define MCOM_NODE_QUEUE_LEN 10
@@ -55,12 +57,12 @@
 
 
 typedef struct _message {
-    unsigned char data [MCOM_DATA_LEN] ;
-    int  expectedChecksum;
-    int  receivedChecksum;
-    int  status;
-    int  transferError;
-    int  destination;
+	unsigned char data [MCOM_DATA_LEN] ;
+	int  expectedChecksum;
+	int  receivedChecksum;
+	int  status;
+	int  transferError;
+	int  destination;
 } message;
 
 typedef struct _McomInPacket {
@@ -91,11 +93,14 @@ typedef struct _McomOutPacket {
 
 #define SIZEOF_MCOM_OUT_PAYLOAD  (MCOM_DATA_LEN+4*sizeof(char))
 #define SIZEOF_MCOM_OUT_HEADER   (2*sizeof(char)+sizeof(word))
+#define SIZEOF_MCOM_OUT_CHK      (2*sizeof(word))
 
 message * outQueues[MCOM_MAX_NODES][MCOM_NODE_QUEUE_LEN]; // ten buffer pointers per device;
-int     snccRequest[MCOM_MAX_NODES]; /// 1 if device requested sncc
 
-message * inQueues [MCOM_MAX_NODES];
+
+message inQueues [MCOM_MAX_NODES];
+int     lastNodeServiced = 0;
+
 
 /* functions */
 void debugMessage(message * m);
@@ -107,17 +112,6 @@ int onMessageReceived(message * q) {
   debugMessage(q);
 }
 
-
-int checkSNCCMessages(McomOutPacket * pck) {
-     
-     unsigned short _signal = *((unsigned short*)&(pck->signalMask2));
-     //if (_signal) {
-       // sncc signal received 
-       printf ("Signal received %x",_signal);
-     //}
-}
-
-
 // if q is null, send sncc request message.
 // if sncc:
 // send a packet with only sncc destination,
@@ -126,99 +120,156 @@ int checkSNCCMessages(McomOutPacket * pck) {
 // wait to be recalled if necessary.
 
 
-int sendMessage(message * inQueue,int * snccRequest, int * pNumSNCCRequests) {
+int sendMessage(message * outQueue,message * inQueues, int * pNumSNCCRequests) {
 
 	McomInPacket pck;
-  (char)* ppck = &pck; 
-	message * outQueue;
+	char* ppck = (char*)&pck; 
+	message * inQueue = NULL;
 
-  pck.preamble_1 = MI_PREAMBLE;
-  pck.preamble_2 = MI_PREAMBLE;
-  pck.cmd = MI_CMD;
+	pck.preamble_1 = MI_PREAMBLE;
+	pck.preamble_2 = MI_PREAMBLE;
+	pck.cmd = MI_CMD;
 
-  // send preamble and get the first answer
-  printBuffer(ppck,SIZEOF_MCOM_OUT_HEADER); bcm2835_spi_transfer (ppck,SIZEOF_MCOM_OUT_HEADER); printBuffer(ppck,SIZEOF_MCOM_OUT_HEADER);
+	// send preamble and get the first answer
+	printBuffer(ppck,SIZEOF_MCOM_OUT_HEADER);  bcm2835_spi_transfern (ppck,SIZEOF_MCOM_OUT_HEADER); printBuffer(ppck,SIZEOF_MCOM_OUT_HEADER);
 
-  // send first bytes to preprocess, if no sncc request is pending,
-  // we'll try to insert the request in this signalmask already
-	preProcessSNCCmessage(&pck,snccRequest,pNumSNCCRequests,outQueue);
-  ppck += SIZEOF_MCOM_OUT_HEADER;
+	// send first bytes to preprocess, if no sncc request is pending,
+	// we'll try to insert the request in this signalmask already
 
-	if (inQueue) {
-		pck.destinationCmd = inQueue->destination;
-		_memcpy(pck.data,inQueue->data,MCOM_DATA_LEN);
-		inQueue->status = MI_STATUS_QUEUED;
-		int checkSum = dataCheckSum(pck.data,MCOM_DATA_LEN);
+	preProcessSNCCmessage(&pck,inQueues,pNumSNCCRequests,&inQueue);
+	ppck += SIZEOF_MCOM_OUT_HEADER;
+
+	int checkSum;
+	if (outQueue) {
+		pck.destinationCmd = outQueue->destination;
+		_memcpy(pck.data,outQueue->data,MCOM_DATA_LEN);
+		outQueue->status = MI_STATUS_QUEUED;
+		checkSum = dataCheckSum(pck.data,MCOM_DATA_LEN);
 		pck.chkSum = checkSum;
 	} else {
-    pck.destinationCmd = 0;
-    pck.chkSum = 0;
-  }
+		pck.destinationCmd = 0;
+		pck.chkSum = 0;
+	}
 
-  if (*pNumSNCCRequests) {
-    pck.destinationSncc = outQueue->destination;
-    outQueue->status = MI_STATUS_QUEUED;
-  } else {
-    pck.destinationSncc = 0;
-    pck.snccCheckSum = 0;
-  }
+	if (inQueue) {
+		pck.destinationSncc = inQueue->destination;
+		inQueue->status = MI_STATUS_QUEUED;
+	} else {
+		pck.destinationSncc = 0;
+		pck.snccCheckSum = 0;
+	}
 
 	pck.__reserved_1 = 0;
-  pck.__reserved_2 = 0;
+	pck.__reserved_2 = 0;
 
-  printBuffer(ppck,SIZEOF_MCOM_OUT_PAYLOAD); bcm2835_spi_transfern (ppck,SIZEOF_MCOM_OUT_PAYLOAD); printBuffer(ppck,SIZEOF_MCOM_OUT_PAYLOAD));
-  ppck += SIZEOF_MCOM_OUT_PAYLOAD;
+	printBuffer(ppck,SIZEOF_MCOM_OUT_PAYLOAD); bcm2835_spi_transfern (ppck,SIZEOF_MCOM_OUT_PAYLOAD); printBuffer(ppck,SIZEOF_MCOM_OUT_PAYLOAD);
+	ppck += SIZEOF_MCOM_OUT_PAYLOAD;
 
-  if (*pNumSNCCRequests) {
-     int checkSumSNCC = dataCheckSum(pck.data,MCOM_DATA_LEN);
-     pck.snccCheckSum = checkSumSNCC;
-  }
+	int checkSumSNCC;
+	if (inQueue) {
+		checkSumSNCC = dataCheckSum(pck.data,MCOM_DATA_LEN);
+		pck.snccCheckSum = checkSumSNCC;
+	}
 
-  printBuffer(ppck,SIZEOF_MCOM_OUT_CHK); bcm2835_spi_transfern (ppck,SIZEOF_MCOM_OUT_CHK);  printBuffer(ppck,SIZEOF_MCOM_OUT_CHK));
+	printBuffer(ppck,SIZEOF_MCOM_OUT_CHK); bcm2835_spi_transfern (ppck,SIZEOF_MCOM_OUT_CHK);  printBuffer(ppck,SIZEOF_MCOM_OUT_CHK);
 
-	if(inQueue && (pck.chkSum == checkSum)) {
+	if(outQueue && (pck.chkSum == checkSum)) {
+		outQueue->status = MI_STATUS_TRANSFERRED;
+	} 
+
+	if (inQueue && (pck.snccCheckSum == checkSumSNCC)) {
+		_memcpy(inQueue->data,pck.data,MCOM_DATA_LEN);
 		inQueue->status = MI_STATUS_TRANSFERRED;
 	} 
 
-  if ((*pNumSNCCRequests) && (pck.snccCheckSum == checkSumSNCC)) {
-     outQueue->status = MI_STATUS_TRANSFERRED;
-  } 
-
-	postProcessSNCCmessage(&pck,inQueue,snccRequest,pNumSNCCRequests,outQueue);      
+	postProcessSNCCmessage(&pck,inQueues,pNumSNCCRequests,inQueue);      
 }
 
 /**
  * Process the pointer list for one node.
  */ 
 int processNodeQueue(message ** q) {
-    int i;
-    // pop first valid pointer which message has been transferred 
-    if ((*q) && (*q)->status == MI_STATUS_TRANSFERRED) {
-      
-        for (i=1;i<MCOM_NODE_QUEUE_LEN;i++) {
-          *q++ = *(q+1);
-        }
-        *q = NULL;
-    } 
- 
+	int i;
+	// pop first valid pointer which message has been transferred 
+	if ((*q) && (*q)->status == MI_STATUS_TRANSFERRED) {
+	  
+		for (i=1;i<MCOM_NODE_QUEUE_LEN;i++) {
+		  *q++ = *(q+1);
+		}
+		*q = NULL;
+	} 
 
 }
 
 // process udp queue.
-
 int insertNewCmds(message ** outQueues) {
-
+ 
 }
 
-        
+		
 // check the snccrequest queue, choose the next node to signal, if any;
-int preProcessSNCCmessage(McomOutPacket* pck, int * snccRequest, int * pNumSNCCRequests,message * outQueue) {
+int preProcessSNCCmessage(McomOutPacket* pck, message * inQueues,int * pNumSNCCRequests,message ** inQueue) {
+
+   if (*pNumSNCCRequests) {
+         // do not favor a particular node, hence loop from
+   	     // lastnodeService to max node, then from 0 to lastnodeSeviced
+   		int i, currentNode;
+   		for (i=lastNodeServiced;i<(MCOM_MAX_NODES+lastNodeServiced);i++) {
+   			currentNode = i%MCOM_MAX_NODES;
+   			if (inQueues[currentNode].status == SNCC_SIGNAL_RECEIVED) {
+   				*inQueue = &(inQueues[currentNode]);
+   				(*inQueue)->destination = currentNode;
+   			}
+   		}
+   } else {
+   		// nodes will squeeze their id in the preamble. 
+   	 	// provided they were alone to do so, we can (if no outqueue)
+   		// already inquiry them in the same packet.
+
+   		if (pck->preamble_1) {
+   			int probableNode = pck->preamble_1 & 0x7F; // strip off nofify bit
+   			if (pck->preamble_1 < MCOM_MAX_NODES) {
+   				*inQueue = &(inQueues[probableNode]);
+   				(*inQueue)->destination = probableNode;
+   				(*inQueue)->status = SNCC_PREAMBLE_RECEIVED;
+   			} 
+   		} 
+   }
 
 }
 
 // update snccrequest queue, in respect to what happened during transfer.
-int postProcessSNCCmessage(McomOutPacket* pck,message *q, int * snccRequest, int * pNumSNCCRequests) {
+int postProcessSNCCmessage(McomOutPacket* pck,message * inQueues,message * inQueue,int * pNumSNCCRequests) {
 
+	  int processedNode = 0;
+	  if (inQueue) {
+	  	 //preprocess assigned a queue, so let's check.
+	  	 if (inQueue->status == MI_STATUS_TRANSFERRED) {
+	  	 		inQueue->status = 0;
+	  	 		processedNode = inQueue->destination;
+	  	 		lastNodeServiced = processedNode;
+	  	 }
+	  }
+
+	  // analyse signalmask.
+	  unsigned int i,j = 1;
+
+	  *pNumSNCCRequests = 0; // recaculate num of open sncc requests
+	  
+	  for(i=0;i<8;i++) {
+	  		if (pck->signalMask1 & j) {
+	  			if (j != inQueue->destination) { // ignore signal of 'just processed' node 
+	  											  // (can't (already) request another one)
+	  				inQueues[j].destination = j;
+     				inQueues[j].status = SNCC_SIGNAL_RECEIVED;
+	  			}
+	  		}
+	  		j <<= 1;
+
+	  		if (inQueues[j].status == SNCC_SIGNAL_RECEIVED) {
+	  			(*pNumSNCCRequests)++;
+	  		}
+	  }
 
 }
 
@@ -238,6 +289,7 @@ int main(int argc, char **argv)
   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW); 
   
   memset(outQueues,0,MCOM_MAX_NODES*MCOM_NODE_QUEUE_LEN*sizeof(message*));
+  memset(inQueues,0,MCOM_MAX_NODES*sizeof(message));
 
   McomOutPacket r;
   int i;
@@ -259,10 +311,10 @@ int main(int argc, char **argv)
 
 	while (1)  {
 
-		while (!insertNewCmds(outQueues)) { // give the opportunity to get new buffers from udp.
+		while (!insertNewCmds((message**)outQueues)) { // give the opportunity to get new buffers from udp.
 			if (bcm2835_gpio_lev(latch)||numSNCCRequests) { // either a node is requesting a sncc packet 
 				//  or we have more snccRequests to service
-				sendMessage(NULL,snccRequest,&numSNCCRequests);
+				sendMessage(NULL,inQueues,&numSNCCRequests);
 			} else {
 				usleep(750);
 			}
@@ -277,7 +329,7 @@ int main(int argc, char **argv)
 				message ** q = outQueues[i]; 
 
 				if (*q != NULL) {
-					sendMessage(*q,snccRequest,&numSNCCRequests);         
+					sendMessage(*q,inQueues,&numSNCCRequests);         
 					if ((*q)->status == MI_STATUS_TRANSFERRED) {
 						processNodeQueue(q); 
 					}
@@ -295,26 +347,26 @@ return 0;
 
 int dataCheckSum (unsigned char * req, int reqLen) {
 
-    // proceess chksum on reqlen, append (2B) chksum after reqLen, return checksum;
-    unsigned short chk = 0;
-    while (reqLen--) {
-       chk += *req++;
-    }
-    //chk = ~chk;
-    return chk;
+	// proceess chksum on reqlen, append (2B) chksum after reqLen, return checksum;
+	unsigned short chk = 0;
+	while (reqLen--) {
+	   chk += *req++;
+	}
+	//chk = ~chk;
+	return chk;
 }
 
 void debugMessage(message * m) {
 
-    unsigned char data [20];
+	unsigned char data [20];
 
-    printf("data: ");
-    printBuffer(m->data,20);
-    printf("expectedChecksum: 0x%x\n",m->expectedChecksum);
-    printf("receivedChecksum: 0x%x\n",m->receivedChecksum);
-    printf("status: 0x%x\n",m->status);
-    printf("transferError: 0x%x\n",m->transferError);
-    printf("destination: 0x%x\n",m->destination);
+	printf("data: ");
+	printBuffer(m->data,20);
+	printf("expectedChecksum: 0x%x\n",m->expectedChecksum);
+	printf("receivedChecksum: 0x%x\n",m->receivedChecksum);
+	printf("status: 0x%x\n",m->status);
+	printf("transferError: 0x%x\n",m->transferError);
+	printf("destination: 0x%x\n",m->destination);
 
 }
 
@@ -322,64 +374,64 @@ int printBuffer (char * b,int size) {
 
   int i;
   for (i=0;i<size;i++) {
-    printf("%x ",b[i]);
+	printf("%x ",b[i]);
   }
   printf("\n");
 
 }
 
 void zeroMem(void * p,int len) {
-     
-     while (len--) {
-        *(unsigned char*)p++ = 0x00;
-     }
+	 
+	 while (len--) {
+		*(unsigned char*)p++ = 0x00;
+	 }
 }
 
 
 void _memcpy( void* dest, void *src, int len) {
 
   while (len--) {
-       *(char*)dest++ = *(char*)src++;
+	   *(char*)dest++ = *(char*)src++;
   }
 }
 
  
 void err(char *str)
 {
-    perror(str);
-    exit(1);
+	perror(str);
+	exit(1);
 }
  
 int lstn(void)
 {
-    struct sockaddr_in my_addr, cli_addr;
-    int sockfd, i; 
-    socklen_t slen=sizeof(cli_addr);
-    char buf[BUFLEN];
+	struct sockaddr_in my_addr, cli_addr;
+	int sockfd, i; 
+	socklen_t slen=sizeof(cli_addr);
+	char buf[BUFLEN];
  
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
-      err("socket");
-    else
-      printf("Server : Socket() successful\n");
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP))==-1)
+	  err("socket");
+	else
+	  printf("Server : Socket() successful\n");
  
-    bzero(&my_addr, sizeof(my_addr));
-    my_addr.sin_family = AF_INET;
-    my_addr.sin_port = htons(PORT);
-    my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    if (bind(sockfd, (struct sockaddr* ) &my_addr, sizeof(my_addr))==-1)
-      err("bind");
-    else
-      printf("Server : bind() successful\n");
+	bzero(&my_addr, sizeof(my_addr));
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(PORT);
+	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	 
+	if (bind(sockfd, (struct sockaddr* ) &my_addr, sizeof(my_addr))==-1)
+	  err("bind");
+	else
+	  printf("Server : bind() successful\n");
  
-    while(1)
-    {
-        if (recvfrom(sockfd, buf, BUFLEN, 0, (struct sockaddr*)&cli_addr, &slen)==-1)
-            err("recvfrom()");
-        printf("Received packet from %s:%d\nData: %s\n\n",
-               inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port), buf);
-    }
+	while(1)
+	{
+		if (recvfrom(sockfd, buf, BUFLEN, 0, (struct sockaddr*)&cli_addr, &slen)==-1)
+			err("recvfrom()");
+		printf("Received packet from %s:%d\nData: %s\n\n",
+			   inet_ntoa(cli_addr.sin_addr), ntohs(cli_addr.sin_port), buf);
+	}
  
-    close(sockfd);
-    return 0;
+	close(sockfd);
+	return 0;
 }
