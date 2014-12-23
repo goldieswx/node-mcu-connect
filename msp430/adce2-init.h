920237d994de7ba48f2e34213d75ad6579c15bb3
goldieswx <goldieswx@gmail.com>
2014-12-23 14:44:30 +0100

    Update adce2-init.h

--- msp430/adce2-init.h
+++ msp430/adce2-init.h
@@ -1,3 +1,24 @@
+/*
+    node-mcu-connect . node.js UDP Interface for embedded devices.
+    Copyright (C) 2013-4 David Jakubowski
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+    
+*/
+    
 #define LOW_POWER_MODE(a)    (__bis_SR_register(LPM3_bits + GIE))
 #define INTERRUPT 			 (P2IN & CS_INCOMING_PACKET)
+
+
+#define IOCFG_HW_ADDR		0x0E000
