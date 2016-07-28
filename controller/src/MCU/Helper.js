6bb83da0cba938398ffe75d4c9c167f4e7f1fb44
goldieswx <goldieswx@bitbucket.org>
2016-07-28 17:50:13 +0200

    Implemeneted HSL helper

--- controller/src/MCU/Helper.js
+++ controller/src/MCU/Helper.js
@@ -12,18 +12,58 @@ var MCUHelper = function(obj) {
 };
 
 
-MCUHelper.prototype.toRGB = function (rgbHexString,intensity){
+MCUHelper.prototype.HSLtoRGB=  function HSLtoRGB(hsl) {
+
+        var h, s, l;
+        h = hsl[0];
+        s = hsl[1];
+        l = hsl[2];
+        var r, g, b;
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+        return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+}
+
+
+MCUHelper.prototype.toHSL = function (hslValues,intensity){
+
+    this.toRGB(this.HSLtoRGB(hslValues,intensity));
+
+};
+
+MCUHelper.prototype.toRGB = function (rgbValues,intensity){
 
 
    var io = this.linkedObject;
 
    if (io.children && io.children.length) {
 
-        var rgbArray = rgbHexString.match(/[0-9A-Za-z]{2}/g);
+        var rgbArray = rgbValues;
+        if (typeof(rgbValues) === 'string') {
+            rgbArray = rgbValues.match(/[0-9A-Za-z]{2}/g);
 
-        rgbArray = _.map(rgbArray,function(value) {
-           return parseInt("0x" + value)/255;
-        });
+            rgbArray = _.map(rgbArray, function (value) {
+                return parseInt("0x" + value) / 255;
+            });
+        }
 
        intensity = intensity || 1.0;
        intensity = Math.min(intensity,1.0);
